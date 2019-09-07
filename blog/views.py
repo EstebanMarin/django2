1a079e7163548aea9dcc3eb6a17a4a53bf5efd67
estebanmarin002@gmail.com <estebanmarin002@gmail.com>
2019-09-06 19:01:51 -0500

    working

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 
 
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(
+        Post,
+        slug=post,
+        status='published',
+        publish__year=year,
+    )
+    return render(request, 'blog/post/detail.html', {'post': post})
+
+
 def post_list(request):
-    post = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)  # 3 post per page
+    page = request.GET.get('page')
+    try:
+        post = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        post = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page results
+        post = paginator.page(paginator.num_pages)
+
+    return render(request, 'blog/post/list.html', {
+        'page': page,
+        'posts': post
+    })
