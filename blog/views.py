bcfd55e9250b1f3171d27625f033b133c4504583
estebanmarin002@gmail.com <estebanmarin002@gmail.com>
2019-09-08 21:31:42 -0500

    taging template working

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,10 @@ from django.core.mail import send_mail
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView
+from taggit.models import Tag
 
-from .forms import EmailPostForm, CommentForm
-from .models import Post, Comment
+from .forms import CommentForm, EmailPostForm
+from .models import Comment, Post
 
 
 def post_share(request, post_id):
@@ -65,10 +66,14 @@ def post_detail(request, year, month, day, post):
         })
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
     paginator = Paginator(object_list, 3)  # 3 post per page
     page = request.GET.get('page')
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
     try:
         post = paginator.page(page)
     except PageNotAnInteger:
@@ -80,7 +85,8 @@ def post_list(request):
 
     return render(request, 'blog/post/list.html', {
         'page': page,
-        'posts': post
+        'posts': post,
+        'tag': tag
     })
 
 
